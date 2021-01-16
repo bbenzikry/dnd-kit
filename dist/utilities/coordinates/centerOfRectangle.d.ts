50d67972cc6eda6072e050fd9b94756e487aec6b
Beni Ben Zikry <bbenzikry@gmail.com>
2021-01-16 23:58:42 +0200

    gitpkg

---
+++ dist/utilities/coordinates/centerOfRectangle.d.ts
@@ -0,0 +1,5 @@
+import type { Coordinates, LayoutRect } from '../../types';
+/**
+ * Returns the coordinates of the center of a given ClientRect
+ */
+export declare function centerOfRectangle(rect: LayoutRect, left?: number, top?: number): Coordinates;
---
+++ dist/utilities/coordinates/index.d.ts
@@ -0,0 +1,5 @@
+export { centerOfRectangle } from './centerOfRectangle';
+export { defaultCoordinates } from './constants';
+export { distanceBetween } from './distanceBetweenPoints';
+export { getEventCoordinates } from './getEventCoordinates';
+export { getRelativeTransformOrigin } from './getRelativeTransformOrigin';
